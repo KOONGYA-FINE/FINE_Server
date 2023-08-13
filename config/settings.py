e50570a4ee4e4db552ff34b43026d22c9af6449e
Yoonho Hann <83288181+hnnynh@users.noreply.github.com>
2023-08-13 18:06:11 +0900

    chore: domain cors

--- config/settings.py
+++ config/settings.py
@@ -86,6 +86,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://fine-lions.com",
+    "https://fine-lions.com/",
 ]
 
 ROOT_URLCONF = "config.urls"
