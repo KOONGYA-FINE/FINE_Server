9fff3e5e79080b80fb98781ae737e3aa97ddc020
hnnynh <hnnynh125@gmail.com>
2023-08-15 14:12:13 +0900

    fix: 사용자 이름 추가

--- places/serializers.py
+++ places/serializers.py
@@ -6,6 +6,7 @@ class PlaceSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only = True)
     name = serializers.CharField(required=True)
     user = serializers.IntegerField(source="user.id", read_only = True)
+    username = serializers.CharField(source="user.username", read_only = True)
     score = serializers.IntegerField(required=True)
     address = serializers.CharField(required=True)
     latitude = serializers.CharField(required=True)
