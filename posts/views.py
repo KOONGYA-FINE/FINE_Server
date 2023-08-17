89483be50db817b44dab9554ab0354f840e32d75
jinyoungpark00 <doghong00@naver.com>
2023-08-17 20:33:39 +0900

    Fix : posts DELETE savedpost 불러오지 못할 시 404

--- posts/views.py
+++ posts/views.py
@@ -342,7 +342,7 @@ class PostDetail(APIView):
         except Exception as e:
             return Response(
                 {"detail": f"An error occurred: {str(e)}"},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                status=status.HTTP_404_NOT_FOUND,
             )
 
 
